9e800e9e17bc6270dde5d844fdab29942eab9593
Jdvxusb <100464172+Jdvxusb@users.noreply.github.com>
2024-09-14 14:12:40 +0300

    Baxkword_elimination

---
+++ Prediction#2/odev_cozum_hazirlik.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 15 04:18:20 2018
+
+@author: sadievrenseker
+"""
+
+#1. kutuphaneler
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#2. Veri Onisleme
+
+#2.1. Veri Yukleme
+veriler = pd.read_csv('odev_tenis.csv')
+#pd.read_csv("veriler.csv")
+
+
+#veri on isleme
+from sklearn import preprocessing
+#encoder:  Kategorik -> Numeric
+from sklearn.preprocessing import LabelEncoder
+veriler2 = veriler.apply(LabelEncoder().fit_transform)
+
+c = veriler2.iloc[:,:1]
+from sklearn.preprocessing import OneHotEncoder
+ohe = preprocessing.OneHotEncoder()
+c=ohe.fit_transform(c).toarray()
+
+havadurumu = pd.DataFrame(data = c, index = range(14), columns=['o','r','s'])
+sonveriler = pd.concat([havadurumu,veriler.iloc[:,1:3]],axis = 1)
+sonveriler = pd.concat([veriler2.iloc[:,-2:],sonveriler], axis = 1)
+
+X = sonveriler.drop('play', axis=1)
+y = sonveriler['play']
+#verilerin egitim ve test icin bolunmesi
+from sklearn.model_selection import train_test_split
+x_train, x_test,y_train,y_test = train_test_split(X,y,test_size=0.33, random_state=0)
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(x_train,y_train)
+
+
+y_pred = regressor.predict(x_test)
+
+print(y_pred)
+print(y_test.values)
+
+
+import statsmodels.api as sm 
+X = np.append(arr = np.ones((14,1)).astype(int), values=X, axis=1 )
+X_l = sonveriler.iloc[:,[0,1,2,3,4,5]].values
+X_l = np.array(X_l,dtype=float)
+model = sm.OLS(y,X_l).fit()
+
+print(model.summary())
+
+
+
+
+
+
+
+
+
